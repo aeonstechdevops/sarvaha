7bb185964e1b4cf9986663a9849fe490dda9b082
gaurav703 <111629507+gaurav703@users.noreply.github.com>
2024-09-03 15:50:43 +0530

    Update mail.utils.ts

--- src/app/lib/mail.utils.ts
+++ src/app/lib/mail.utils.ts
@@ -14,7 +14,7 @@ export const sendMail = async (body: string) => {
   try {
     await transporter.sendMail({
       from: "sarvah@sggs.ac.in",
-      to: ["dean.iic@sggs.ac.in", "sarvah@sggs.ac.in"],
+      to: ["dean.iil@sggs.ac.in", "sarvah@sggs.ac.in"],
       subject: "IMPORTANT | Contacted from Sarvah Website ",
       html: body,
     });
